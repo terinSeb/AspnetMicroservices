818999b306a8a0fac5c8829027dcdea62453a72f
Terin Sebastin <terin01@gmail.com>
2022-12-13 05:54:37 +0530

    Running Catalog API

--- src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -9,7 +9,7 @@ namespace Catalog.API.Repositories
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
-        Task<Product> GetProduct(string id);;
+        Task<Product> GetProduct(string id);
         Task<IEnumerable<Product>> GetProductByName(string name);
         Task<IEnumerable<Product>> GetProductByCategory(string CategoryName);
         Task CreateProduct(Product product);
