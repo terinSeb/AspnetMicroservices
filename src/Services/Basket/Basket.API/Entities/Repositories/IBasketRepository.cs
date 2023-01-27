dce7496430faa6ec9f6deb4419c19099a182385e
Terin Sebastin <terin01@gmail.com>
2023-01-27 08:02:25 +0530

    Connecting with Redis Database and creating Repository

---
+++ src/Services/Basket/Basket.API/Entities/Repositories/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
