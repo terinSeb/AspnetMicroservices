d030e644b0e65e1c6503566ffd7ed00362bdfd82
Terin Sebastin <terin01@gmail.com>
2023-02-23 06:20:45 +0530

    Completed Discount Controller

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -26,5 +26,24 @@ namespace Discount.API.Controllers
             var coupon = await _repository.GetDiscount(productName);
             return Ok(coupon);
         }
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { ProductName = coupon.ProductName }, coupon);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _repository.UpdateDiscount(coupon));
+        }
+        [HttpDelete("{productName}",Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscount(productName));
+        }
     }
 }
